0b7247e92358bbce056af4100e6417c8c00e9f37
sseezov <sseezov@mail.ru>
2025-01-20 14:57:01 +0700

    2 module solution

--- server/server.js
+++ server/server.js
@@ -6,16 +6,29 @@ await client.connect()
 
 fastify.get('/partners', async (request, reply) => {
   try {
-    const res = await client.query(`SELECT T1.*,
-    CASE WHEN sum(T2.production_quantity) > 300000 THEN 15
-    WHEN sum(T2.production_quantity) > 50000 THEN 10
-    WHEN sum(T2.production_quantity) > 10000 THEN 5
-    ELSE 0 
-    END as discount
-    from partners as T1
-    JOIN sales as T2 on T1.id = T2.partner_id
-    GROUP BY T1.id`)
-    await reply.send(res.rows)
+    const partners = await client.query(`SELECT * FROM partners;`)
+    const sales = await client.query(`SELECT * FROM sales;`)
+
+    const partnersWithDiscount = partners.rows.map((partner) => {
+      const total = sales.rows.reduce((acc, elem) => {
+        if (elem.partner_id === partner.id) {
+          return acc + Number(elem.production_quantity)
+        }
+        return acc
+      });
+
+      let discount = 0;
+
+      if (total > 300000) {
+        discount = 15
+      } else if (total > 50000) {
+        discount = 10
+      } else if (total > 10000) {
+        discount = 5
+      }
+      return { ...partner, discount }
+    })
+    await reply.send(partnersWithDiscount)
   } catch (e) {
     console.log(e)
   }
